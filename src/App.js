748fdb160f9f7b2a22b1642e1723939d3f92ef63
TAKZOBYE <takerchanel@gmail.com>
2021-08-14 20:32:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Counter from './Counter';
 import './App.css';
 
 function App() {
+
+  const [show, setShow] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { show && <Counter /> }
+      <button onClick={() => setShow(!show)}>Toggle</button>
     </div>
   );
 }
